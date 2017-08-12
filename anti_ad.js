507d15af6335fa74aaf6e73fab00ea08f7d3ee47
Tommy <Tommy>
2017-08-11 20:40:47 -0700

    site blocker: Tommy is not allowed to visit certain webpages

--- anti_ad.js
+++ anti_ad.js
@@ -332,6 +332,56 @@ var f = function() {
         es[i].currentTime = es[i].duration;
       } catch(_) {}
   }
+
+  // Determine whether to disable this webpage
+  {
+    var disable = false;
+
+    // Disable /r/factorio
+    var es = document.getElementsByTagName('em');
+    for(var i=0; i<es.length; ++i) {
+      if(es[i].innerText === "Namaste. You seek balance. Here is my wisdom. Your mistakes have no cost but time, and the deconstruction planner even reduces that cost. Most games punish you for building, demolishing and rebuilding. Not Factorio. Let your anxiety wash away as you perceive that every belt placed can be moved. Every assembler is but a visitor to where it resides. The only significance is life, which leads to the further wisdom. Look both ways before you cross the tracks.")
+        disable = true;
+    }
+
+    // Disable /r/ffviiremake
+    if(window.getComputedStyle(document.body, false).backgroundImage === 'url("https://a.thumbs.redditmedia.com/mgKSOsATIw9wFPOwGjD3cR9IyN2zD7ZJZ9iO3ooT2D4.png")')
+      disable = true;
+
+    // Disable twitch.tv
+    if(window.location.hostname.indexOf('twitch.tv') >= 0)
+      disable = true;
+
+    // Disable some youtube channels
+    if(window.location.hostname.indexOf('youtube.com') >= 0) {
+      var youtube_blacklist = ['NintendoCapriSun', 'EthosLab'];
+
+      for(var k=0; k<youtube_blacklist.length; ++k) {
+        var ch = youtube_blacklist[k];
+
+        // Disable youtube videos from those channels
+        var ess = document.getElementsByClassName('g-hovercard');
+        for(var j=0; j<ess.length; ++j)
+          if(ess[j].innerText === ch)
+            disable = true;
+
+        // Disable other pages pertaining to those youtube channels
+        if(window.location.href.toUpperCase().indexOf(ch.toUpperCase()) >= 0)
+          disable = true;
+      }
+    }
+  }
+
+//  console.log('stupid');
+
+  // Disable if necessary
+  if(disable) {
+    clearTimeout(timeout_handle);
+    document.head.remove();
+    document.body.remove();
+    document.children[0].appendChild(document.createElement('body'));
+    document.body.appendChild(document.createTextNode('Tommy has decided that this page cannot be viewed.'));
+  }
 };
 
 // Call f() every once in a while
