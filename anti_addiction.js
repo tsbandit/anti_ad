65c11bdfb08d988eb43ad4b1b282f81c223a65d3
Tommy <Tommy>
2023-05-09 22:05:00 -0700

    refactor by creating function make_state()

--- anti_addiction.js
+++ anti_addiction.js
@@ -3,25 +3,26 @@
 
 const Global = window.Global = window.Global || {};
 
-const {sleep} = Global.util;
+const {sleep, make_state} = Global.util;
 
 Global.anti_addiction = async() => {
-  let disable = null;
+  console.log('in anti_addiction()');
+
+  const [get_is_disabled, set_is_disabled] = make_state(false);
+  const disable = set_is_disabled;
 
   const determine_whether_to_disable_this_webpage = () => {
-    disable = false;
-
 /*
     // Disable /r/factorio
     es = document.getElementsByTagName('em');
     for(var i=0; i<es.length; ++i) {
       if(es[i].innerText === "Namaste. You seek balance. Here is my wisdom. Your mistakes have no cost but time, and the deconstruction planner even reduces that cost. Most games punish you for building, demolishing and rebuilding. Not Factorio. Let your anxiety wash away as you perceive that every belt placed can be moved. Every assembler is but a visitor to where it resides. The only significance is life, which leads to the further wisdom. Look both ways before you cross the tracks.")
-        disable = {reason: 'factorio subreddit'};
+        disable({reason: 'factorio subreddit'});
     }
 
     // Disable /r/ffviiremake
     if(window.getComputedStyle(document.body, false).backgroundImage === 'url("https://a.thumbs.redditmedia.com/mgKSOsATIw9wFPOwGjD3cR9IyN2zD7ZJZ9iO3ooT2D4.png")')
-      disable = {reason: 'ffviiremake subreddit'};
+      disable({reason: 'ffviiremake subreddit'});
 */
 
     // Disable certain hostnames
@@ -30,7 +31,7 @@ Global.anti_addiction = async() => {
         ||  window.location.hostname.indexOf('factorio.com') >= 0
         ||  window.location.hostname.indexOf('reddit.com') >= 0
         )
-      disable = {reason: 'banned hostname'};
+      disable({reason: 'banned hostname'});
 
     // Disable some youtube channels
     if(window.location.hostname.indexOf('youtube.com') >= 0) {
@@ -43,11 +44,11 @@ Global.anti_addiction = async() => {
         ess = document.getElementsByClassName('g-hovercard');
         for(var j=0; j<ess.length; ++j)
           if(ess[j].innerText === ch)
-            disable = {reason: 'username: ' + ch};
+            disable({reason: 'username: ' + ch});
 
         // Disable other pages pertaining to those youtube channels
         if(window.location.href.toUpperCase().indexOf(ch.toUpperCase()) >= 0)
-          disable = {reason: 'url contains: ' + ch};
+          disable({reason: 'url contains: ' + ch});
       }
     }
   };
@@ -55,28 +56,32 @@ Global.anti_addiction = async() => {
   const only_disable_at_certain_times_of_day = () => {
     const time = new Date().getHours();
     if(time >= 12  &&  time < 22)
-      disable = false;
+      set_is_disabled(false);
   };
 
-  // Disable if necessary
-  const disable_if_necessary = () => {
-    if(disable) {
-      console.log('disabling because: ' + disable.reason);
-      clearTimeout(timeout_handle);
-      if(document.head !== null) {
-        document.head.remove();
-      }
-      document.body.remove();
-      document.children[0].appendChild(document.createElement('body'));
-      document.body.appendChild(document.createTextNode('Tommy has decided that this page cannot be viewed.'));
+  const enact_the_disabling = (reason) => {
+    console.log('disabling because: ' + reason);
+    clearTimeout(timeout_handle);
+    if(document.head !== null) {
+      document.head.remove();
     }
+    document.body.remove();
+    document.children[0].appendChild(document.createElement('body'));
+    document.body.appendChild(document.createTextNode('Tommy has decided that this page cannot be viewed.'));
   };
 
   const main = async() => {
     determine_whether_to_disable_this_webpage();
+
     only_disable_at_certain_times_of_day();
-    await(sleep(0));
-    disable_if_necessary();
+
+    await sleep(0);
+
+    const is_disabled = get_is_disabled();
+
+    if(is_disabled) {
+      enact_the_disabling(is_disabled.reason);
+    }
   };
 
   await main();
--- util.js
+++ util.js
@@ -7,7 +7,25 @@ const sleep = (n) => new Promise((resolve, reject) => {
   setTimeout(resolve, n);
 });
 
-Global.util = {sleep};
+const make_state = (initial_value) => {
+  if(typeof initial_value === 'function')
+    throw new Error('initial_value should not be a function');
+
+  let state = initial_value;
+  const get_state = () => {
+    return state;
+  };
+  const set_state = (arg) => {
+    if(typeof arg === 'function') {
+      state = arg(state);
+    } else {
+      state = arg;
+    }
+  };
+  return [get_state, set_state];
+};
+
+Global.util = {sleep, make_state};
 
 
 })();
