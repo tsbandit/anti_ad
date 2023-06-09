42c656ac43d6ea008cd7a0ffc368702d49a21ee6
Tommy <Tommy>
2023-06-09 16:00:50 -0700

    disable test_notifications()

--- main_background_script.js
+++ main_background_script.js
@@ -5,10 +5,9 @@
 
 const Global = window.Global = window.Global || {};
 
-const {test_notifications} = Global.test_notifications;
+//const {test_notifications} = Global.test_notifications;
 
 const main = async() => {
-  await test_notifications();
 };
 
 main();
