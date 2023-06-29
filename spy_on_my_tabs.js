14803e92f098477b2f747ee6b0af07799367650f
Tommy <Tommy>
2023-06-28 22:45:33 -0700

    now the api_key is persistent

--- spy_on_my_tabs.js
+++ spy_on_my_tabs.js
@@ -5,7 +5,7 @@
 
 const Global = window.Global = window.Global || {};
 
-const {sleep, escalate} = Global.util;
+const {storage_get, storage_set, sleep, escalate} = Global.util;
 
 const get_tabs = () => (new Promise((resolve) => {chrome.tabs.query({}, resolve);}));
 
@@ -36,11 +36,16 @@ const spy_on_my_tabs = async() => {
   const test_string = await (await fetch('http://localhost:6002')).text();
   console.log({test_string});
 
+  window.api_key = await storage_get('api_key');
+
   while(true) {
     try {
       if(window.api_key === undefined) {
         console.log('waiting for api_key');
+        (async() => {throw new Error('missing api_key');})();  // Create notification
       } else {
+        storage_set({api_key: window.api_key});  // Save it for later.
+
         const response = await call_api({
           type: 'save browser tabs',
           api_key: window.api_key,
