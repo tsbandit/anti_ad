2e6cd62c29cd2bd49cd22e357c3c395ad4692562
Tommy <Tommy>
2025-04-27 08:30:29 -0700

    tweak anti_addiction.js

--- anti_addiction.js
+++ anti_addiction.js
@@ -12,85 +12,85 @@ const anti_addiction = async() => {
   const [get_is_disabled, set_is_disabled] = make_state(false);
   const disable = set_is_disabled;
 
-  const disable_factorio_subreddit = () => {
-    const es = document.getElementsByTagName('em');
-    for(var i=0; i<es.length; ++i) {
-      if(es[i].innerText === "Namaste. You seek balance. Here is my wisdom. Your mistakes have no cost but time, and the deconstruction planner even reduces that cost. Most games punish you for building, demolishing and rebuilding. Not Factorio. Let your anxiety wash away as you perceive that every belt placed can be moved. Every assembler is but a visitor to where it resides. The only significance is life, which leads to the further wisdom. Look both ways before you cross the tracks.")
-        disable({reason: 'factorio subreddit'});
-    }
-  };
+//  const disable_factorio_subreddit = () => {
+//    const es = document.getElementsByTagName('em');
+//    for(var i=0; i<es.length; ++i) {
+//      if(es[i].innerText === "Namaste. You seek balance. Here is my wisdom. Your mistakes have no cost but time, and the deconstruction planner even reduces that cost. Most games punish you for building, demolishing and rebuilding. Not Factorio. Let your anxiety wash away as you perceive that every belt placed can be moved. Every assembler is but a visitor to where it resides. The only significance is life, which leads to the further wisdom. Look both ways before you cross the tracks.")
+//        disable({reason: 'factorio subreddit'});
+//    }
+//  };
 
-  const disable_ffviiremake_subreddit = () => {
-    if(window.getComputedStyle(document.body, false).backgroundImage === 'url("https://a.thumbs.redditmedia.com/mgKSOsATIw9wFPOwGjD3cR9IyN2zD7ZJZ9iO3ooT2D4.png")')
-      disable({reason: 'ffviiremake subreddit'});
-  };
+//  const disable_ffviiremake_subreddit = () => {
+//    if(window.getComputedStyle(document.body, false).backgroundImage === 'url("https://a.thumbs.redditmedia.com/mgKSOsATIw9wFPOwGjD3cR9IyN2zD7ZJZ9iO3ooT2D4.png")')
+//      disable({reason: 'ffviiremake subreddit'});
+//  };
 
-/*
-  const disable_certain_hostnames = () => {
-    if(       window.location.hostname.indexOf('twitch.tv') >= 0
-          ||  window.location.hostname.indexOf('factorio.com') >= 0
-          ||  window.location.hostname.indexOf('reddit.com') >= 0
-          )
-      disable({reason: 'banned hostname'});
-  };
-*/
+//  const disable_certain_hostnames = () => {
+//    if(       false
+//          ||  window.location.hostname.indexOf('twitch.tv') >= 0
+//          ||  window.location.hostname.indexOf('factorio.com') >= 0
+//          ||  window.location.hostname.indexOf('reddit.com') >= 0
+//          )
+//      disable({reason: 'banned hostname'});
+//  };
 
-  const disable_some_youtube_channels = () => {
-    if(window.location.hostname.indexOf('youtube.com') >= 0) {
-      var youtube_blacklist = ['NintendoCapriSun', 'EthosLab'];
+//  const disable_some_youtube_channels = () => {
+//    if(window.location.hostname.indexOf('youtube.com') >= 0) {
+//      var youtube_blacklist = ['NintendoCapriSun', 'EthosLab'];
+//
+//      for(var k=0; k<youtube_blacklist.length; ++k) {
+//        var ch = youtube_blacklist[k];
+//
+//        // Disable youtube videos from those channels
+//        const ess = document.getElementsByClassName('g-hovercard');
+//        for(var j=0; j<ess.length; ++j)
+//          if(ess[j].innerText === ch)
+//            disable({reason: 'username: ' + ch});
+//
+//        // Disable other pages pertaining to those youtube channels
+//        if(window.location.href.toUpperCase().indexOf(ch.toUpperCase()) >= 0)
+//          disable({reason: 'url contains: ' + ch});
+//      }
+//    }
+//  };
 
-      for(var k=0; k<youtube_blacklist.length; ++k) {
-        var ch = youtube_blacklist[k];
+//  const disable_ally = () => {
+//    if(window.location.hostname.indexOf('ally.com') >= 0) {
+//      disable({reason: 'ally.com is banned right now'});
+//    }
+//  };
 
-        // Disable youtube videos from those channels
-        const ess = document.getElementsByClassName('g-hovercard');
-        for(var j=0; j<ess.length; ++j)
-          if(ess[j].innerText === ch)
-            disable({reason: 'username: ' + ch});
+//  const disable_vanguard = () => {
+//    if(window.location.hostname.indexOf('vanguard.com') >= 0) {
+//      disable({reason: 'vanguard is banned right now'});
+//    }
+//  };
 
-        // Disable other pages pertaining to those youtube channels
-        if(window.location.href.toUpperCase().indexOf(ch.toUpperCase()) >= 0)
-          disable({reason: 'url contains: ' + ch});
-      }
-    }
-  };
-
-  const disable_ally = () => {
-    if(window.location.hostname.indexOf('ally.com') >= 0) {
-      disable({reason: 'ally.com is banned right now'});
-    }
-  };
-
-  const disable_vanguard = () => {
-    if(window.location.hostname.indexOf('vanguard.com') >= 0) {
-      disable({reason: 'vanguard is banned right now'});
-    }
-  };
-
-  const disable_finance_yahoo = () => {
-    if(window.location.hostname.indexOf('finance.yahoo.com') >= 0) {
-      disable({reason: 'finance.yahoo.com is banned right now'});
-    }
-  };
+//  const disable_finance_yahoo = () => {
+//    if(window.location.hostname.indexOf('finance.yahoo.com') >= 0) {
+//      disable({reason: 'finance.yahoo.com is banned right now'});
+//    }
+//  };
 
   const determine_whether_to_disable_this_webpage = () => {
-    disable_ally();
-    disable_vanguard();
-    disable_finance_yahoo();
+    //disable_ally();
+    //disable_vanguard();
+    //disable_finance_yahoo();
+    //disable_certain_hostnames();
   };
 
-  const only_disable_at_certain_times_of_day = () => {
-    const time = new Date().getHours();
-    if(time >= 12  &&  time < 22)
-      set_is_disabled(false);
-  };
+//  const only_disable_at_certain_times_of_day = () => {
+//    const time = new Date().getHours();
+//    if(time >= 12  &&  time < 22)
+//      set_is_disabled(false);
+//  };
 
-  const only_disable_on_certain_dates = () => {
-    const time = Date.now();
-    const reenablement_date = new Date('2023-06-02T00:00:00.000');
-    if(time > reenablement_date)
-      set_is_disabled(false);
-  };
+//  const only_disable_on_certain_dates = () => {
+//    const time = Date.now();
+//    const reenablement_date = new Date('2023-06-02T00:00:00.000');
+//    if(time > reenablement_date)
+//      set_is_disabled(false);
+//  };
 
   const enact_the_disabling = (reason) => {
     console.log('disabling because: ' + reason);
@@ -106,7 +106,7 @@ const anti_addiction = async() => {
   const main = async() => {
     determine_whether_to_disable_this_webpage();
 
-    only_disable_on_certain_dates();
+//    only_disable_on_certain_dates();
 
     await sleep(0);
 
