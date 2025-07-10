6cada9d5390129428a9e7869207cba553dbada6c
Tommy <Tommy>
2025-07-10 00:30:13 -0700

    update rip_bestplaces.js

--- rip_bestplaces.js
+++ rip_bestplaces.js
@@ -66,12 +66,70 @@ const maybe_process_politics = async(candidate) => {
       pathname: window.location.pathname,
       name: match[1],
       dem: match[2],
-      rep: match[2],
+      rep: match[3],
+      bugfixed: true,
+    },
+  });
+};
+
+const regexp_4 = /^Housing costs in (.*)\?(?:x|[^x])*A typical home costs \$([0-9,\.]*), which is (?:x|[^x])*\. Renting a two-bedroom unit in .* costs \$([0-9,\.]*) per month, which is/;
+const maybe_process_cost = async(candidate) => {
+  const match = candidate.innerText.match(regexp_4);
+  if(match === null)
+    return;
+
+  await call_api_stupidly({
+    type: 'save arbitrary data',
+    data: {
+      type: 'bestplaces',
+      subtype: 'housing',
+      pathname: window.location.pathname,
+      name: match[1],
+      buy: match[2],
+      rent: match[3],
+    },
+  });
+};
+
+const regexp_5 = /The population in (.*) is ([0-9,\.]+). There are ([0-9,\.]+) people per square mile aka population density. The median age in (?:.*) is ([0-9,\.]+), the US median age is 38.4. The number of people per household in (?:.*) is ([0-9,\.]+), the US average of people per household is 2.6.\n\nFamily in (?:.*)\n- ([0-9,\.]+)% are married\n- ([0-9,\.]+)% are divorced\n- ([0-9,\.]+)% are married with children\n- ([0-9,\.]+)% have children, but are single\nRace in (?:.*)\n\n- ([0-9,\.]+)% are White\n- ([0-9,\.]+)% are Black\n- ([0-9,\.]+)% are Asian\n- ([0-9,\.]+)% are Native American\n- ([0-9,\.]+)% claim Other\n- ([0-9,\.]+)% claim Hispanic ethnicity\n- ([0-9,\.]+)% Two or more races\n- ([0-9,\.]+)% Hawaiian, Pacific Islander/;
+const maybe_process_people = async(candidate) => {
+  const match = candidate.innerText.match(regexp_5);
+  if(match === null)
+    return;
+
+  await call_api_stupidly({
+    type: 'save arbitrary data',
+    data: {
+      type: 'bestplaces',
+      subtype: 'people',
+      pathname: window.location.pathname,
+      name: match[1],
+      population: match[2],
+      density: match[3],
+      median_age: match[4],
+      people_per_household: match[5],
+      percentage_married: match[6],
+      percentage_divorced: match[7],
+      percentage_married_with_children: match[8],
+      percentage_single_with_children: match[9],
+      percentage_race_white: match[10],
+      percentage_race_black: match[11],
+      percentage_race_asian: match[12],
+      percentage_race_native_american: match[13],
+      percentage_race_other: match[14],
+      percentage_race_hispanic: match[15],
+      percentage_race_mixed: match[16],
+      percentage_race_pacific_islander: match[17],
     },
   });
 };
 
 const rip_data = () => {
+  // Only rip data when looking at certain URLs:
+  const path_parts = window.location.pathname.split('/');
+  if(path_parts.length < 3  ||  path_parts[2] !== 'city')
+    return;
+
   window.addEventListener('load', async(ev) => {
     const candidates_h5 = document.querySelectorAll('h5');
     for(const candidate of candidates_h5) {
@@ -82,6 +140,12 @@ const rip_data = () => {
     const candidates_p = document.querySelectorAll('p');
     for(const candidate of candidates_p) {
       await maybe_process_politics(candidate);
+    }
+
+    const candidates_div_card_body = document.querySelectorAll('div.card-body');
+    for(const candidate of candidates_div_card_body) {
+      await maybe_process_cost(candidate);
+      await maybe_process_people(candidate);
     }
   });
 };
